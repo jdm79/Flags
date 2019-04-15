0090636f9b6535f256c9ebc398ab753d3b3b2cbf
jdm79 <james.malvern@gmail.com>
2019-04-15 17:46:56 -0500

    first commit: creates navigation controller, uses table view

---
+++ Flags/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flags
+//
+//  Created by James Daniel Malvern on 15/04/2019.
+//  Copyright © 2019 Malvernation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
