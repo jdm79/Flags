b837103d8101e861c373d2e359606a761067c1bb
jdm79 <james.malvern@gmail.com>
2019-04-15 18:32:05 -0500

    feat: adds countries array, lists countries in rows, adds nav controller

--- Flags/ViewController.swift
+++ Flags/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var countries = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        cell.textLabel?.text = countries[indexPath.row]
+        return cell
+    }
 
 }
 
